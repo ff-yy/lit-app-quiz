ba699c886e92a7ee13c13b7ae8f7aa79bc368962
ff-yy <yuya12121@gmail.com>
2023-05-04 21:55:09 +0900

    Initial Commit

---
+++ Quiz/QuizViewController.swift
@@ -0,0 +1,82 @@
+//
+//  QuizViewController.swift
+//  Quiz
+//
+//  Created by litech on 2015/02/10.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    //クイズを格納する配列
+    var quizArray = [[Any]]()
+    
+    //正解数
+    var correctAnswer: Int = 0
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons1: UIButton!
+    @IBOutlet var choiceButtons2: UIButton!
+    @IBOutlet var choiceButtons3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //------------------------ここから下にクイズを書く------------------------//
+        quizArray.append(["宇治抹茶で有名な宇治市があるのはどこでしょうか？", "奈良県", "栃木県", "京都府", 3])
+        quizArray.append(["日本にお茶を飲む文化が広まったのはいつ頃でしょうか？", "鎌倉時代", "江戸時代", "昭和時代", 1])
+        quizArray.append(["次のうち、茶道の場でマナー違反な服装はどれでしょうか？", "ワンピース", "スーツ", "ミニスカート", 3])
+        quizArray.append(["茶道部がある高校の割合はどの程度でしょうか？", "約20%", "約40%", "約60%", 3])
+        
+        quizArray.append(["抹茶の日はいつでしょうか？", "1/30", "2/6", "5/2", 2])
+        
+        //------------------------ここから上にクイズを書く------------------------//
+        
+        //quizArrayの中身をシャッフルする
+        quizArray.shuffle()
+
+        choiceQuiz()
+    }
+    
+    func choiceQuiz() {
+        quizTextView.text = quizArray[0][0] as? String
+        
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        choiceButtons1.setTitle(quizArray[0][1] as? String, for: .normal)
+        choiceButtons2.setTitle(quizArray[0][2] as? String, for: .normal)
+        choiceButtons3.setTitle(quizArray[0][3] as? String, for: .normal)
+    }
+    
+    @IBAction func choiceAnswer(sender: UIButton) {
+        if quizArray[0][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer+=1
+        }
+        
+        quizArray.remove(at: 0)
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if quizArray.count == 0 {
+            performSegueToResult()
+        } else {
+            choiceQuiz()
+        }
+    }
+    
+    func performSegueToResult() {
+        performSegue(withIdentifier: "toResultView", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toResultView") {
+            
+            let resultView = segue.destination as! ResultViewController
+            resultView.correctAnswer = self.correctAnswer
+        }
+    }
+}
+
+
